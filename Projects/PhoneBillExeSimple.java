d189dc1c11f192e5f37d1087cab8deadcefa7f5e
Krishna Panchal <147529256+krishhpancha@users.noreply.github.com>
2026-03-30 12:03:06 +0530

    Delete Projects directory

--- Projects/PhoneBillExeSimple.java
+++
@@ -1,9 +0,0 @@
-package chapter6;
-
-public class phonebillexesimple {
-    public static void main(String arg[]){
-        SimplePhoneBill exe1 =new SimplePhoneBill();
-        exe1.setMinutesUsed(1000);
-        exe1.printbill();
-    }
-}
